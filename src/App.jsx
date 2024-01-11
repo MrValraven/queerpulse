c69bb138ed4caa63350db9bfb427c75d7399e1e9
mrvalraven <akatiago@gmail.com>
2024-01-11 20:04:45 +0000

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Logo from './assets/logo.png'
+import Logo from './assets/Logo.png'
 import './App.css'
 
 function App() {
